18d6338d0978fa4c897b07bd575876c60aa0ce54
sokcuri <sokcuri@gmail.com>
2020-04-12 08:26:07 +0900

    initial commit

---
+++ interface.ts
@@ -0,0 +1,23 @@
+import HaruBase from '.';
+import discord from 'discord.js';
+
+export * from '.';
+
+export interface Module {
+    active: boolean;
+    name: string;
+
+    channels: string[];
+
+    doMessage: (harubase: HaruBase, message: discord.Message, commandPair?: CommandPair) => boolean;
+}
+
+export interface CommandPair {
+    command: string;
+    arguments?: string[];
+}
+
+export interface HaruBaseArgs {
+    discordKey: string;
+    modulePath: string;
+}
